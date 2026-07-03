64ae444f9de49d82cdc842e24f2e1b1d0d16d9c2
varshithachegonda <24wh1a05x9@bvrithyderabad.edu.in>
2026-07-03 14:46:35 +0530

    initial add

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app=express()
-const PORT = 3002;
+const PORT = process.env.PORT||3002
 
 app.use(express.static("frontend"))
-
 
 app.listen(PORT,function(){
     console.log("successfully running at http://localhost:"+PORT)
