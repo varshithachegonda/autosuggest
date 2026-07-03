cd4677e9c4f15576f1f0a776ff5bedc3be5e0227
varshithachegonda <24wh1a05x9@bvrithyderabad.edu.in>
2026-07-03 18:40:44 +0530

    initial commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,26 @@
-console.log("hello from js");
-alert("hello from js");
+
+const users = [
+  {
+    "name" :"Jane Doe",
+    "gender" : "Female",
+    "image" : "../images/jane.png"
+  },
+  {
+    "name" :"John Doe",
+    "gender" : "Male",
+    "image" : "../images/john.png"
+  },
+]
+let curUserId = 0;
+function toggle(){
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+  // image
+  document.getElementById("img").src=users[curUserId].image;
+  // name
+  document.getElementById("card-name").innerText=users[curUserId].name;
+}
+
+console.log("Hello from JS");
