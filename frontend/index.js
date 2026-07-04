311e12049d5aabf2e11b85b592b6dfcbbf16a02b
varshithachegonda <24wh1a05x9@bvrithyderabad.edu.in>
2026-07-04 10:18:10 +0530

    initial add

--- frontend/index.js
+++ frontend/index.js
@@ -14,8 +14,7 @@ const users = [
 let curUserId = 0;
 function toggle(){
   if(curUserId == 0)
-    curUserId=1;
-  else
+    curUserId=1;      
     curUserId=0;
   // image
   document.getElementById("img").src=users[curUserId].image;
@@ -24,3 +23,27 @@ function toggle(){
 }
 
 console.log("Hello from JS");
+function getRandomUser()
+{
+  fetch("https://randomuser.me/api")
+    .then(function (data){
+      return data.json()
+    })
+    .then(function (parsedData){
+      // console.log(parsedData)
+      let gender = parsedData.results[0].gender;
+
+      let first = parsedData.results[0].name.first;
+      let last = parsedData.results[0].name.last;
+      let name = first  + " " + last;
+      let imageUrl = parsedData.results[0].picture.large;
+      // gender
+      document.getElementById("card-gender").innerText=gender;
+      // name
+      document.getElementById("card-name").innerText=name
+      // img
+      document.getElementById("img").src=imageUrl
+
+    })
+}
+getRandomUser();
